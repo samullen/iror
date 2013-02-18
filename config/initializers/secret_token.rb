a925e64234f647923db7a1dd36ec4ab659d90e2a
Samuel Mullen <samullen@gmail.com>
2013-02-18 09:21:18 -0600

    initial offering

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Iror::Application.config.secret_token = 'd8cf34df037543ee79373acc2975222ecbd95204f5c18d91620cba130bd14dfce1c256001cc0f6949cf2cdedf4ddcb1167dd9777114bce18c9903bac9d660352'
